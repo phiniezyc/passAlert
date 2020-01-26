a6f9abb04b28809a6de3eed67e8594a13b140ec5
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-26 13:06:01 -0500

    name boilerplate

--- Models/HelloViewModel.cs
+++ Models/HelloViewModel.cs
@@ -11,6 +11,16 @@ namespace AvaloniaHelloViewModels.Models
 
     public string Greeting {
       get {return _greeting;}
+      set {
+        if(value !=_greeting) {
+          _greeting = value;
+          onPropertyChanged();
+        }
+      }
+    }
+
+    public string Name {
+     get {return _name;}
     }
     public event PropertyChangedHandler PropertyChanged;
     protected virtual void onPropertyChanged([CallerMemberName] string propertyName = null)
