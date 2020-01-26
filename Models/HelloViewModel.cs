9207af5461f8a72c469bd979428d3b51199937f2
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-26 13:00:47 -0500

    Add private vars

--- Models/HelloViewModel.cs
+++ Models/HelloViewModel.cs
@@ -6,8 +6,9 @@ namespace AvaloniaHelloViewModels.Models
 {
   class HelloViewModel : INotifyPropertyChanged
   {
+    private string _greeting;
+    private string _name;
     public event PropertyChangedHandler PropertyChanged;
-
     protected virtual void onPropertyChanged([CallerMemberName] string propertyName = null)
     {
       PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
